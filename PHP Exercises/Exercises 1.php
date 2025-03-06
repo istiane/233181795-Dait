851207354a45d187e4af860681cfb55c328ce1ec
istiane <cdd1795@students.uc-bcf.edu.ph>
2025-03-06 07:53:44 +0000

    2

---
+++ PHP Exercises/Exercises 1.php
@@ -0,0 +1,16 @@
+<?php
+$a = 15;
+$b = 7;
+
+$sum = $a + $b;
+$difference = $a - $b;
+$product = $a * $b;
+$quotient = $a / $b;
+
+echo "a = " . $a . "\n";
+echo "b = " . $b . "\n";
+echo "Sum of a and b: " . $sum . "\n";
+echo "Difference of a and b: " . $difference . "\n";
+echo "Product of a and b: " . $product . "\n";
+echo "Quotient of a and b: " . $quotient . "\n";
+?>
